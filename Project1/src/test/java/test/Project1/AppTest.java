8d6421639bc604c9eb5d24fe434bc1d8c3f2213c
Akila <akilashreega@gmail.com>
2023-05-06 12:25:43 -0400

    firstCommit

---
+++ Project1/src/test/java/test/Project1/AppTest.java
@@ -0,0 +1,18 @@
+package test.Project1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("project1");
+    }
+}
